17b65e586420db3b0b7617ea57a4b60ff908d8da
Барин <163689215+Bismarck09@users.noreply.github.com>
2026-06-15 13:24:20 +0300

    QuestUpdate

--- Assets/CyberClub/Scripts/Quests/Data/QuestData.cs
+++ Assets/CyberClub/Scripts/Quests/Data/QuestData.cs
@@ -4,10 +4,34 @@ using UnityEngine;
 public class QuestData : ScriptableObject
 {
     [SerializeField] private QuestType _questType;
+    [SerializeField] private string _descriptionTemplate;
     [SerializeField] private int _targetValue;
     [SerializeField] private int _rewardValue;
-    
+
     public QuestType Type => _questType;
     public int TargetValue => _targetValue;
     public int RewardValue => _rewardValue;
+
+    public string GetDescription()
+    {
+        if (string.IsNullOrWhiteSpace(_descriptionTemplate))
+            return GetDefaultDescription();
+
+        return string.Format(_descriptionTemplate, _targetValue);
+    }
+
+    private string GetDefaultDescription()
+    {
+        switch (_questType)
+        {
+            case QuestType.VisitorService:
+                return $"Обслужи {_targetValue} клиентов";
+
+            case QuestType.BuyDevice:
+                return $"Купи {_targetValue} компьютеров";
+
+            default:
+                return $"Выполни действие {_targetValue} раз";
+        }
+    }
 }
--- Assets/CyberClub/Scripts/Quests/UI/QuestUI.cs
+++ Assets/CyberClub/Scripts/Quests/UI/QuestUI.cs
@@ -2,51 +2,75 @@ using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
 
-public class QuestUI  : MonoBehaviour
+public class QuestUI : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _description;
     [SerializeField] private TextMeshProUGUI _reward;
     [SerializeField] private GameObject _startPanel;
     [SerializeField] private Button _completePanel;
     [SerializeField] private Image _progressBar;
-    
+
     private Quest _quest;
 
     public void Init(QuestData data)
     {
-        _description.text = $"Обслужи {data.TargetValue} клиентов";
+        _description.text = data.GetDescription();
         _reward.text = data.RewardValue.ToString();
+
+        _completePanel.gameObject.SetActive(false);
+
+        if (_progressBar != null)
+            _progressBar.fillAmount = 0f;
     }
 
     public void Activate(Quest quest)
     {
-        _startPanel.gameObject.SetActive(false);
-        
+        _startPanel.SetActive(false);
+
         _quest = quest;
-        
+
         _quest.OnProgressChanged += UpdateUI;
         _quest.OnCompleted += CompleteQuest;
-        
+
+        _completePanel.onClick.RemoveListener(ClaimReward);
         _completePanel.onClick.AddListener(ClaimReward);
     }
 
     private void CompleteQuest()
     {
         _completePanel.gameObject.SetActive(true);
-        
+
         _quest.OnCompleted -= CompleteQuest;
         _quest.OnProgressChanged -= UpdateUI;
     }
 
     private void ClaimReward()
     {
+        if (_quest == null)
+            return;
+
         _quest.ResourceData.AddResource(_quest.GetData().RewardValue, 1);
-        
+
+        _completePanel.onClick.RemoveListener(ClaimReward);
+
         Destroy(gameObject);
     }
-    
+
     private void UpdateUI(int currentProgress, int targetProgress)
     {
+        if (_progressBar == null || targetProgress <= 0)
+            return;
+
         _progressBar.fillAmount = (float)currentProgress / targetProgress;
+    }
+
+    private void OnDestroy()
+    {
+        if (_quest == null)
+            return;
+
+        _quest.OnCompleted -= CompleteQuest;
+        _quest.OnProgressChanged -= UpdateUI;
+        _completePanel.onClick.RemoveListener(ClaimReward);
     }
 }
